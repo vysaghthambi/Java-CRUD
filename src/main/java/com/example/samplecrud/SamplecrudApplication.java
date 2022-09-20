b3551079c146a350a658f56e137744e43d5cf2c5
Vysagh <vthambi@korewireless.com>
2022-09-20 14:09:53 +0530

    first commit

---
+++ src/main/java/com/example/samplecrud/SamplecrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplecrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samplecrud/SamplecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
