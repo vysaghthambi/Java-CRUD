b48a8a8245f24eaaf8f0160010d62a456c188396
Vysagh <vthambi@korewireless.com>
2022-09-20 14:52:49 +0530

    Setup controller

---
+++ src/main/java/com/example/samplecrud/controller/TodoController.java
@@ -0,0 +1,29 @@
+package com.example.samplecrud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.samplecrud.model.TodoDTO;
+import com.example.samplecrud.repository.TodoRepository;
+
+@RestController
+public class TodoController {
+
+    @Autowired
+    private TodoRepository todoRepo;
+
+    @GetMapping("/todos")
+    public ResponseEntity<?> getAllTodos() {
+        List<TodoDTO> todos = todoRepo.findAll();
+        if (todos.size() > 0) {
+            return new ResponseEntity<>(todos, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("No todos available", HttpStatus.NOT_FOUND);
+        }
+    }
+}
