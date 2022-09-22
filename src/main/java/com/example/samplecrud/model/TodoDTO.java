304850632a069937f94404d557f727e29964f244
Vysagh <vthambi@korewireless.com>
2022-09-22 12:07:53 +0530

    sample commit

--- src/main/java/com/example/samplecrud/model/TodoDTO.java
+++ src/main/java/com/example/samplecrud/model/TodoDTO.java
@@ -2,6 +2,8 @@ package com.example.samplecrud.model;
 
 import java.util.Date;
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -19,10 +21,18 @@ public class TodoDTO {
 
     @Id
     private String id;
+
+    @NotNull(message = "todo cannot be null")
     private String todo;
+
+    @NotNull(message = "description cannot be null")
     private String description;
+
+    @NotNull(message = "completed cannot be null")
     private Boolean completed;
+
     private Date createdAt;
+
     private Date updatedAt;
 
 }
